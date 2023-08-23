f87fb0d15d8f025b13ee943536a91dfe96cd2f4f
Tezzish <ishanpahwa14@gmail.com>
2023-08-24 00:12:46 +0200

    adds more frontend features

---
+++ spotvenv/playlist_analyser/playlist_analysis/static/analysisscript.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function() {
+    const spinnerContainer = document.querySelector('.spinner_container');
+    spinnerContainer.style.display = 'none';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const spinnerContainer = document.querySelector('.spinner_container');
+    spinnerContainer.style.display = 'flex';
+});
+
+const toggleButton = document.getElementById('down_button');
+const attributeContainer = document.getElementById('attribute_container');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const image = document.getElementById('album_art');
+
+toggleButton.addEventListener('click', function() {
+    attributeContainer.style.display = 'block';
+    toggleButton.style.display = 'none';
+    title.style.display = 'none';
+    author.style.display = 'none';
+    // change size of image
+    image.style.width = '50%';
+    image.style.height = '50%';
+});
--- spotvenv/playlist_analyser/playlist_analysis/tests.py
+++ spotvenv/playlist_analyser/playlist_analysis/tests.py
@@ -8,24 +8,24 @@ class PlaylistHandlerTestCase(TestCase):
     def setUp(self):
         self.handler = PlaylistHandler()
 
-    # def test_get_playlist_from_url(self):
-    #     # gets a json of the playlist
-    #     playlist_url = "https://open.spotify.com/playlist/6cUbe8r2kP140bL0Z2HHXV?si=89cbce6452b84b10"
-    #     playlist = self.handler.get_playlist_from_url(playlist_url)
-    #     self.assertIsNotNone(playlist)
-    #     self.assertEqual(playlist['name'], "Fleece it out")
-    #     self.assertEqual(html.unescape(playlist['description']), "I should've burned this place down when I had the chance")
-    #     self.assertEqual(playlist['owner']['display_name'], "Ishan")
-    #     self.assertEqual(playlist['images'][0]['url'], "https://mosaic.scdn.co/640/ab67616d0000b27337b16a48dec737a46b244ea2ab67616d0000b273586acdba3a0ddc93693a313eab67616d0000b273a6b996b1140c516deda4b9b0ab67616d0000b273cab7ae4868e9f9ce6bdfdf43")
-    #     self.assertEqual(len(playlist['tracks']['items']), 19)
-    #     print(playlist)
+    def test_get_playlist_from_url(self):
+        # gets a json of the playlist
+        playlist_url = "https://open.spotify.com/playlist/6cUbe8r2kP140bL0Z2HHXV?si=89cbce6452b84b10"
+        playlist = self.handler.get_playlist_from_url(playlist_url)
+        self.assertIsNotNone(playlist)
+        self.assertEqual(playlist['name'], "Fleece it out")
+        self.assertEqual(html.unescape(playlist['description']), "I should've burned this place down when I had the chance")
+        self.assertEqual(playlist['owner']['display_name'], "Ishan")
+        self.assertEqual(playlist['images'][0]['url'], "https://mosaic.scdn.co/640/ab67616d0000b27337b16a48dec737a46b244ea2ab67616d0000b273586acdba3a0ddc93693a313eab67616d0000b273a6b996b1140c516deda4b9b0ab67616d0000b273cab7ae4868e9f9ce6bdfdf43")
+        self.assertEqual(len(playlist['tracks']['items']), 19)
+        print(playlist)
         
 
-    # def test_get_playlist_id_from_link(self):
-    #     playlist_id = self.handler.get_playlist_id_from_link("https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M")
-    #     self.assertEqual(playlist_id, "37i9dQZF1DXcBWIGoYBM5M")
-    #     with_q_mark = self.handler.get_playlist_id_from_link("https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M?si=3e2e2e2e2e2e2e2e")
-    #     self.assertEqual(with_q_mark, "37i9dQZF1DXcBWIGoYBM5M")
+    def test_get_playlist_id_from_link(self):
+        playlist_id = self.handler.get_playlist_id_from_link("https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M")
+        self.assertEqual(playlist_id, "37i9dQZF1DXcBWIGoYBM5M")
+        with_q_mark = self.handler.get_playlist_id_from_link("https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M?si=3e2e2e2e2e2e2e2e")
+        self.assertEqual(with_q_mark, "37i9dQZF1DXcBWIGoYBM5M")
 
     def test_get_playlist(self):
         playlist_url = "https://open.spotify.com/playlist/6cUbe8r2kP140bL0Z2HHXV?si=89cbce6452b84b10"
