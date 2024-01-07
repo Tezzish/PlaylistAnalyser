a039e2a58804fe368c4cbd937e84f16bb12c16f6
Tezzish <ishanpahwa14@gmail.com>
2024-01-07 11:50:48 +0100

    Refactoring form method

--- frontend/src/Form.jsx
+++ frontend/src/Form.jsx
@@ -1,31 +1,15 @@
 import { useState } from 'react';
 
+const PlaylistURL = "http://localhost:8000/analysis/"
+
 function Form() {
   const [text, setText] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await fetch('http://localhost:8000/analysis/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 'playlist_link': text }),
-        credentials: 'include',
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+    MakeRequest(PlaylistURL, text );
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -40,6 +24,28 @@ function Form() {
       <button type="submit">Submit</button>
     </form>
   );
+}
+
+async function MakeRequest(url, link) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 'playlist_link': link }),
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
 export default Form;
