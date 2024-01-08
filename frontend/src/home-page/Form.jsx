415f7faf86b7662f2e93b145bb421fed2b92f353
Tezzish <ishanpahwa14@gmail.com>
2024-01-08 22:51:44 +0100

    Adding more styles to the application

--- frontend/src/home-page/Form.jsx
+++ frontend/src/home-page/Form.jsx
@@ -5,28 +5,45 @@ const PlaylistURL = "http://localhost:8000/analysis/"
 
 function Form() {
   const [text, setText] = useState('');
+  const [invalidInput, setInvalidInput] = useState(false);
   const navigate = useNavigate();
+
+  function validateInput(input) {
+    // Check if input is a valid Spotify playlist link
+    const regex = /^https:\/\/open\.spotify\.com\/playlist\/\S+\?si=\S+$/;
+    return regex.test(input);
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (!validateInput(text)) {
+      setInvalidInput(true);
+      setTimeout(() => setInvalidInput(false), 820);
+      return;
+    }
     const responseFromServer = await MakeRequest(PlaylistURL, text );
     navigate('/analysis/', { state: { data: responseFromServer } });
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Text:
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-      </label>
-      <button type="submit">Submit</button>
+      <div className="form-group">
+        <label>
+          Enter your Spotify playlist link:
+        </label>
+        <div className="form-control">
+          <input
+            type="text"
+            className={`form-input ${invalidInput ? 'shake-input' : ''}`}
+            placeholder="https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF?si=3d87034436a84644"
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button className={`form-button ${invalidInput ? 'shake-button' : ''}`} type="submit">Submit</button>
+        </div>  
+      </div>
     </form>
   );
-}
+  }
 
 async function MakeRequest(url, link) {
   try {
--- frontend/src/home-page/HomePage.jsx
+++ frontend/src/home-page/HomePage.jsx
@@ -1,11 +1,12 @@
 import Form from './Form.jsx';
+import './HomePage.css';
 
 function HomePage() {
   return (
-    <>
-        <h1>Spotify Playlist Analyzer</h1>
-        <Form />
-    </>
+    <div className="container">
+        <h1 id="home-page-header">Spotify Playlist Analyzer</h1>
+        <Form className="form"/>
+    </div>
   );
 }
 
